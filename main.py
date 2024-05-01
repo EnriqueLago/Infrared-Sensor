104558d48525ae07d9e2b0b88d96a9f25e3861a2
srPepo <enriquelago2b@gmail.com>
2024-05-01 16:29:19 +0200

    Fixed exit error, format of the code

--- main.py
+++ main.py
@@ -1,81 +1,48 @@
 import asyncio
 import random
-import nats
 import logging
 import os
 import sys
 import argparse
+import nats 
+from nats.errors import ConnectionClosedError, TimeoutError
 
-# Create the "logs" directory if it does not exist
-logs_dir = os.path.join(os.getcwd(), 'logs')
-os.makedirs(logs_dir, exist_ok=True)
 
-# Configure the output of logs to the "sensor_log.log" file inside the "logs" folder
-log_file = os.path.join(logs_dir, 'sensor_log.log')
-logging.basicConfig(filename=log_file, level=logging.INFO)
+# --------------------AUXILIAR FUNCTIONS------------------------- #
+# Simulates the operation of the infrared sensor
+def generate_sensor_data(min_value, max_value):
+    data = []
+    for _ in range(64):
+        datum = random.randint(min_value, max_value)
+        data.append(datum)
+    return data
 
+# Función que modifica la variable global
+def modify_global_variable():
+    global continue_program  # Declaración global solo necesaria una vez en la función
+    continue_program = False
+
+# Displays the values of the sensor on the screen with decorative border
+def display_data(data):
+    data_str = ", ".join(str(datum) for datum in data)
+    terminal_width = os.get_terminal_size().columns
+    border = "#" + "-" * (terminal_width - 2) + "#"
+    print(border)
+    print(f"   [Datos: {{{data_str}}}]\n{border}")
+
+# -------------------------MAIN CODE--------------------------#
 async def main(args):
-    if args.sensor_type == 'real':
-        print("Attention! The real sensor is not implemented yet.")
-        while True:
-            user_input = input("Do you want to change to the mockup? (Y/N): ").strip().lower()
-            if user_input in ("y", "n"):
-                break
-            else:
-                print("Please enter Y or N.")
-        if user_input == "y":
-            args.sensor_type = 'mockup'
-        else:
-            print("The program will close")
-            sys.exit()
-    
-     # Controls the min and max values
-    if args.min_value < 0:
-        args.min_value = 0
-    if args.max_value > 65535:
-        args.max_value = 65535
-    if args.min_value > args.max_value:
-        print("The minimum value cannot be greater than the maximum value.")
-        sys.exit()
-
-    # Connect to NATS server asynchronously
-    try:
-        nc = nats.NATS()
-        await nc.connect()
-    except Exception as e:  # Catch any connection-related errors
-        logging.error(f"Failed to connect to NATS server: {e}")
-        sys.exit(1)
-
-    # Variable to track whether to display sensor data
-    display_data_flag = False
-
-    # Event to signal the start or stop of displaying data
-    display_event = asyncio.Event()
-
-    # Simulates the operation of the infrared sensor
-    def generate_sensor_data(min_value, max_value):
-        data = []
-        for _ in range(64):
-            datum = random.randint(min_value, max_value)
-            data.append(datum)
-        return data
-
-    # Displays the values of the sensor on the screen with decorative border
-    def display_data(data):
-        data_str = ", ".join(str(datum) for datum in data)
-        terminal_width = os.get_terminal_size().columns
-        border = "#" + "-" * (terminal_width - 2) + "#"
-        print(border)
-        print(f"   [Datos: {{{data_str}}}]\n{border}")
-
+    #--------------------EVENT HANDLERS-----------------------#
     # Handles the incoming start/stop/exit message and starts/stops/displaying data or exits accordingly
     async def handle_instructions(msg):
         nonlocal display_data_flag
         instruction = msg.data.decode()
+        
         if instruction == "start":
             logging.info("Received start instruction")
             display_data_flag = True
             display_event.set()  # Set the event to start displaying data
+        
         elif instruction == "stop":
             logging.info("Received stop instruction")
             if display_data_flag:  # Solo permite "stop" si ya está en curso "start"
@@ -96,42 +63,110 @@ async def main(args):
                 else:
                     print("Please enter Y or N.")
             if user_input == "n":
-                await nc.close()
-                sys.exit()
+                modify_global_variable()
+                return
             print("Waiting for instructions...")
             
     # Start displaying data every two seconds
     async def start_displaying_data():
         while True:
-            # Wait for the event to be set (start displaying data) or cleared (stop displaying data)
+            # Waits for the event to be set (start displaying data) or cleared (stop displaying data)
             await display_event.wait()
             if display_data_flag:
-                # Generate data
-                data = generate_sensor_data(args.min_value, args.max_value)
+                data = generate_sensor_data(args.min_value, args.max_value) # Generate data
+                display_data(data) 
+                await asyncio.sleep(args.reading_frequency) # Waits as many seconds as indicated by the user (with the arg reading_frequency)
+     
+    #------------------------------MAIN---------------------------#
+    # Variable to control whether to continue with the program
+    global continue_program 
+    continue_program = True
+    
+    # Warns that the real sensor functioning is not implemented yet and offers to change to mockup            
+    if args.sensor_type == 'real':
+        print("Attention! The real sensor functioning is not implemented yet.")
+        while True:
+            user_input = input("Do you want to change to the mockup? (Y/N): ").strip().lower()
+            if user_input in ("y", "n"):
+                break
+            else:
+                print("Please enter Y or N.")
+        if user_input == "y":
+            args.sensor_type = 'mockup'
+        else:
+            print("The program will close")
+            sys.exit()
+    
+    # Controls the min and max values
+    if args.min_value < 0:
+        args.min_value = 0
+    if args.max_value > 65535:
+        args.max_value = 65535
+    if args.min_value > args.max_value:
+        print("The minimum value cannot be greater than the maximum value.")
+        sys.exit()
 
-                # Display data
-                display_data(data)
+    # Connect to NATS server asynchronously
+    try:
+        nc = await asyncio.wait_for(nats.connect(), timeout=10) # Connects to de default port nats://localhost:4222
+    except ConnectionClosedError:
+        print("Connection to NATS server refused. Please make sure the server is running and reachable.")
+        sys.exit()
+    except TimeoutError:
+        print("Connection to NATS server timed out. Please check your network connection and try again.")
+        sys.exit()
+    except Exception as e:  # Catch any other unexpected errors
+        print(f"Failed to connect to NATS server: {e}")
+        sys.exit()
 
-                # Wait 2 seconds
-                await asyncio.sleep(args.reading_frequency)
+    # Variable to track whether to display sensor data
+    display_data_flag = False
     
     # Subscribe to the topic asynchronously
     await nc.subscribe("sensor_instructions", cb=handle_instructions)
 
     print("Waiting for instructions...")
+  
+    # Event to signal the start or stop of displaying data
+    display_event = asyncio.Event()
 
     # Start the task to display data
     asyncio.create_task(start_displaying_data())
 
     # Process messages indefinitely within the async event loop
-    await asyncio.Future()  # Placeholder to keep the program running
+    try:
+        while continue_program:
+            await asyncio.sleep(0.1)  # Brief pause to avoid consuming too many resources
+    finally:
+        # Close the connection before exiting the program if continue_program is False
+        if not continue_program:
+            await nc.close()
 
 if __name__ == "__main__":
+    
+    # Create an ArgumentParser object for parsing command-line arguments
     parser = argparse.ArgumentParser(description='Sensor parameters control')
+    
+    # Add command-line arguments for specifying sensor type, reading frequency, minimum value, and maximum value
     parser.add_argument('--sensor-type', type=str, choices=['mockup', 'real'], default='mockup', help='Type of sensor to use (mockup or real)')
     parser.add_argument('--reading-frequency', type=int, default=2, help='Frequency of sensor readings in seconds')
     parser.add_argument('--min-value', type=int, default=0, help='Minimum value generated by the infrared sensor when it is of mockup type')
     parser.add_argument('--max-value', type=int, default=65535, help='Maximum value generated by the infrared sensor when it is of mockup type')
+
+    # Parse the command-line arguments and store them in the 'args' variable
     args = parser.parse_args()
+    
+    # Check if sensor type is 'real' and if so, ensure that min and max values are not provided
+    if args.sensor_type == 'real':
+        if args.min_value != 0 or args.max_value != 65535:
+            parser.error("For 'real' sensor type, minimum and maximum values should not be provided.")
+            
+    # Create the "logs" directory if it does not exist
+    logs_dir = os.path.join(os.getcwd(), 'logs')
+    os.makedirs(logs_dir, exist_ok=True)
+
+    # Configure the output of logs to the "sensor_log.log" file inside the "logs" folder
+    log_file = os.path.join(logs_dir, 'sensor_log.log')
+    logging.basicConfig(filename=log_file, level=logging.INFO)
     
     asyncio.run(main(args))
